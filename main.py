997d921a915278cd317d3da546f06588770276ed
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-12-14 15:38:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+number = int(input("Enter binary number:"))
+
+def convert(n):
+    if n == 1:
+        for i in range(1, i-1):
+            i=2
+            n = n*2^2
+            n + n
+    print(n)
+
+print(convert(number))
